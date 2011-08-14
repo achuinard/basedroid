4b11028bb85527ebdaf1be2c25885410ef1fceb3
Tony Chuinard <tony.chuinard@gmail.com>
2011-08-14 14:20:17 -0400

    Adding proj

---
+++ src/com/princhu/HttpConnectionManager.java
@@ -0,0 +1,43 @@
+package com.princhu;
+
+import org.apache.http.HttpVersion;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+
+/**
+ * achuinard
+ */
+public class HttpConnectionManager {
+    private static HttpClient INSTANCE;
+
+    public static HttpClient getInstance() {
+        if (INSTANCE == null) {
+            final HttpParams params = new BasicHttpParams();
+            final SchemeRegistry registry = new SchemeRegistry();
+            final SSLSocketFactory sslSocketFactory = SSLSocketFactory.getSocketFactory();
+
+            HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+            HttpProtocolParams.setContentCharset(params, "utf-8");
+            HttpProtocolParams.setUserAgent(params, "App Kingdoms Android");
+
+            sslSocketFactory.setHostnameVerifier(SSLSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER);
+
+            registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+            registry.register(new Scheme("https", sslSocketFactory, 443));
+
+            ClientConnectionManager manager = new ThreadSafeClientConnManager(params, registry);
+            INSTANCE = new DefaultHttpClient(manager, params);
+        }
+
+        return INSTANCE;
+    }
+}
---
+++ src/com/princhu/UnregisteredUserActivity.java
@@ -0,0 +1,28 @@
+package com.princhu;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+import com.google.inject.Inject;
+import roboguice.activity.RoboActivity;
+import roboguice.util.Ln;
+
+public class UnregisteredUserActivity extends RoboActivity {
+
+    @Inject
+    private SharedPreferences sharedPreferences;
+
+    @Inject
+    private PrinchuClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView view = new TextView(this);
+        view.setText(String.valueOf(sharedPreferences.getBoolean("test", false)));
+
+        setContentView(view);
+
+        Ln.v("Sample GET: %s", client.exampleJsonSearch());
+    }
+}
